2b2e3e4c45e78a28043de8d29facdc48c6fce8e3
MaxTeenspirit <maximkaproduction1@gmail.com>
2024-06-04 21:46:09 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/flashcards-maker/sw.js', { scope: '/flashcards-maker/' })})}
